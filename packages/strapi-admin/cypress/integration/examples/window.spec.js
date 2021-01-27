73fc37a3bdea15aa68d0b4222b71f914c03ff9b2
HichamELBSI <elabbassih@gmail.com>
2021-01-27 17:28:14 +0100

    Init Cypress
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

---
+++ packages/strapi-admin/cypress/integration/examples/window.spec.js
@@ -0,0 +1,26 @@
+/* eslint-disable strict */
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+context('Strapi auth', () => {
+  // beforeEach(cy.setup);
+  // afterEach(cy.reset);
+
+  beforeEach(() => {
+    cy.visit('http://localhost:4000/admin');
+  });
+
+  it('login', () => {
+    cy.task('reset', { tata: 2 }).then((args) => console.log(args));
+    cy.findByLabelText('Email')
+      .should('exist')
+      .type('test@test.fr');
+    cy.findByLabelText('Mot de Passe')
+      .should('exist')
+      .type('mypassword');
+    cy.findByRole('button', { name: 'Se connecter' })
+      .should('exist')
+      .click();
+    cy.findByText('Invalid credentials').should('be.visible');
+  });
+});
---
+++ packages/strapi-admin/cypress/plugins/index.js
@@ -0,0 +1,26 @@
+/* eslint-disable strict */
+/* eslint-disable no-unused-vars */
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+  on('task', {
+    reset(args) {
+      console.log(args);
+    }
+  })
+}
